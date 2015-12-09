d6e6aa70db2e46633b8d9754f18fa29e3d3b8dec
AnthonyX25 <asuro1man@hotmail.com>
2015-12-09 13:03:27 -0600

    CX25

---
+++ inc/head.php
@@ -0,0 +1,14 @@
+<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
+    <!-- use jssor.slider.mini.js (40KB) instead for release -->
+    <!-- jssor.slider.mini.js = (jssor.js + jssor.slider.js) -->
+    <script type="text/javascript" src="/js/jssor.js"></script>
+    <script type="text/javascript" src="/js/jssor.slider.js"></script>
+	<script type="text/javascript" src="js/js.js"></script>
+	<script type="text/javascript" src="js/jsslider.js"></script>
+  <style>
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+      width: 70%;
+      margin: auto;
+  }
+  </style>
